f220ffe149a9481e3feaa9a9f237f06e3d6f25c8
prerana1821 <prerananawar1@gmail.com>
2023-07-24 06:18:56 +0530

    chore: removed unwanted files

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const { initializeDBConnection } = require("./db/db.connect");
 const resume = require("./routes/resumes.router");
 const aisuggestions = require("./routes/aisuggestions.router");
-// const user = require("./routes/user.router");
-// const category = require("./routes/categories.router");
+
 const { errorHandler } = require("./middlewares/error-handler.middleware");
 const { routeNotFound } = require("./middlewares/route-not-found.middleware");
 
 const app = express();
 const PORT = 4000;
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -18,12 +19,9 @@ initializeDBConnection();
 
 app.use("/resumes", resume);
 app.use("/ai-suggestions", aisuggestions);
-// app.use("/products", product);
-// app.use("/categories", category);
-// app.use("/user-details", authVerify, user);
 
 app.get("/", (req, res) => {
-  res.json({ success: true, data: "Hello preStore!" });
+  res.json({ success: true, data: "Hello skillful CV!" });
 });
 
 app.use(routeNotFound);
